fcdcded0c09f917b110a2837941870716d469b72
Cory Kilger <cmkilger@gmail.com>
2011-05-21 18:48:30 -0700

    New ImageAdditions Xcode project added to the Examples workspace.

---
+++ ImageAdditionsTests/ImageAdditionsTests.h
@@ -0,0 +1,17 @@
+//
+//  ImageAdditionsTests.h
+//  ImageAdditionsTests
+//
+//  Created by Cory Kilger on 5/21/11.
+//  Copyright 2011 Cory Kilger. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ImageAdditionsTests : SenTestCase {
+@private
+    
+}
+
+@end
